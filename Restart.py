9143e351618ee799a08dd7b096a3f9936d86c114
Lucas <92604093+iLucasRT@users.noreply.github.com>
2022-03-21 18:05:15 -0300

    Delete Restart.py

--- Restart.py
+++
@@ -1,8 +0,0 @@
-def restart():
-    import sys
-    print("argv was",sys.argv)
-    print("sys.executable was", sys.executable)
-    print("restart now")
-
-    import os
-    os.execv(sys.executable, ['python'] + sys.argv)
